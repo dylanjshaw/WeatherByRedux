d4f50401f423510ca28471e84536903fe3520390
dylanjshaw <dylanjshaw@sandiego.edu>
2017-04-28 10:02:57 -0700

    render sparklines chart for temperature

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 class WeatherList extends React.Component {
+  renderWeather(cityData){
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp)
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Sparklines data={temps} width={180} height={120} margin={5}>
+            <SparklinesLine color="purple" />
+          </Sparklines>
+        </td>
+      </tr>
+
+    );
+  }
+
   render(){
     return (
       <table className="table table-hover">
@@ -15,6 +33,7 @@ class WeatherList extends React.Component {
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
